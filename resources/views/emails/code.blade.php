7a8a4f071b948af112594a8abc5727526d209592
Norman <normansprks@gmail.com>
2023-10-05 01:06:47 -0500

    Up

---
+++ resources/views/emails/code.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PROYECTO 1 SIST. OPERATIVOS</title>
+</head>
+<body>
+<h1>{{ $title }}</h1>
+<p>Su codigo es : {{ $code }}</p>
+
+<p>Gracias.</p>
+</body>
+</html>
